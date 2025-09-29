bfc38f9907d72eeeee1cb03134271f4c99fe3b23
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2025-09-29 02:42:07 +0000

    x

--- test/test_fizz_buzz.py
+++ test/test_fizz_buzz.py
@@ -1,11 +1,16 @@
 from fizz_buzz import fizz_buzz
 
+def test_fizz_buzz():
+    assert fizz_buzz(15) == 'fizz buzz'
+
 def test_fizz():
     assert fizz_buzz(3) == 'fizz'
 
 def test_buzz():
     assert fizz_buzz(5) == 'buzz'
 
-def test_fizz_buzz():
-    fizz_buzz(15)
-    assert True
+def test_otherwise():
+    # assert fizz_buzz(1) == '1'
+    # fizz_buzz(1)
+    # assert True
+    pass
